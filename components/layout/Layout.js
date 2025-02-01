d2365a5d96febdeff23fe8c2fad449c512347bf4
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-02-01 10:33:11 +0330

    add layout to project

---
+++ components/layout/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href="/">
+          <h2>SIRAF CAR</h2>
+          <p>Choose and Buy your car</p>
+        </Link>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>
+        <a href="/" target="_blank">
+          ehsanatashkar.js
+        </a>{" "}
+        Next.js courses | CarStore Project &copy;
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
